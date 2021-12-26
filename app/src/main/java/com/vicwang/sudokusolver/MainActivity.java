2aeb2a6c6195ad93479d26f92e94d25f5f5f7590
Vic Wang <vic.leiwang@gmail.com>
2021-12-26 10:40:43 +0000

    Use OnClickListener

--- app/src/main/java/com/vicwang/sudokusolver/MainActivity.java
+++ app/src/main/java/com/vicwang/sudokusolver/MainActivity.java
@@ -6,7 +6,7 @@ import android.widget.Button;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
     private SudokuBoard gameBoard;
     private Solver gameBoardSolver;
     private Button solveBTN;
@@ -19,52 +19,27 @@ public class MainActivity extends AppCompatActivity {
         gameBoard = findViewById(R.id.SudokuBoard);
         gameBoardSolver = gameBoard.getSolver();
 
+        Button num1BTN = findViewById(R.id.button1);
+        Button num2BTN = findViewById(R.id.button2);
+        Button num3BTN = findViewById(R.id.button3);
+        Button num4BTN = findViewById(R.id.button4);
+        Button num5BTN = findViewById(R.id.button5);
+        Button num6BTN = findViewById(R.id.button6);
+        Button num7BTN = findViewById(R.id.button7);
+        Button num8BTN = findViewById(R.id.button8);
+        Button num9BTN = findViewById(R.id.button9);
         solveBTN = findViewById(R.id.solveButton);
-    }
 
-    public void BTNOnePress(View view) {
-        gameBoardSolver.setNumberPos(1);
-        gameBoard.invalidate();
-    }
-
-    public void BTNTwoPress(View view) {
-        gameBoardSolver.setNumberPos(2);
-        gameBoard.invalidate();
-    }
-
-    public void BTNThreePress(View view) {
-        gameBoardSolver.setNumberPos(3);
-        gameBoard.invalidate();
-    }
-
-    public void BTNFourPress(View view) {
-        gameBoardSolver.setNumberPos(4);
-        gameBoard.invalidate();
-    }
-
-    public void BTNFivePress(View view) {
-        gameBoardSolver.setNumberPos(5);
-        gameBoard.invalidate();
-    }
-
-    public void BTNSixPress(View view) {
-        gameBoardSolver.setNumberPos(6);
-        gameBoard.invalidate();
-    }
-
-    public void BTNSevenPress(View view) {
-        gameBoardSolver.setNumberPos(7);
-        gameBoard.invalidate();
-    }
-
-    public void BTNEightPress(View view) {
-        gameBoardSolver.setNumberPos(8);
-        gameBoard.invalidate();
-    }
-
-    public void BTNNinePress(View view) {
-        gameBoardSolver.setNumberPos(9);
-        gameBoard.invalidate();
+        num1BTN.setOnClickListener(this);
+        num2BTN.setOnClickListener(this);
+        num3BTN.setOnClickListener(this);
+        num4BTN.setOnClickListener(this);
+        num5BTN.setOnClickListener(this);
+        num6BTN.setOnClickListener(this);
+        num7BTN.setOnClickListener(this);
+        num8BTN.setOnClickListener(this);
+        num9BTN.setOnClickListener(this);
+        solveBTN.setOnClickListener(this);
     }
 
     public void solve(View view) {
@@ -77,6 +52,32 @@ public class MainActivity extends AppCompatActivity {
         } else {
             solveBTN.setText(R.string.Solve);
             gameBoardSolver.resetBoard();
+        }
+    }
+
+    @Override
+    public void onClick(View view) {
+        int id = view.getId();
+        if (id == R.id.button1) {
+            gameBoardSolver.setNumberPos(1);
+        } else if (id == R.id.button2) {
+            gameBoardSolver.setNumberPos(2);
+        } else if (id == R.id.button3) {
+            gameBoardSolver.setNumberPos(3);
+        } else if (id == R.id.button4) {
+            gameBoardSolver.setNumberPos(4);
+        } else if (id == R.id.button5) {
+            gameBoardSolver.setNumberPos(5);
+        } else if (id == R.id.button6) {
+            gameBoardSolver.setNumberPos(6);
+        } else if (id == R.id.button7) {
+            gameBoardSolver.setNumberPos(7);
+        } else if (id == R.id.button8) {
+            gameBoardSolver.setNumberPos(8);
+        } else if (id == R.id.button9) {
+            gameBoardSolver.setNumberPos(9);
+        } else if (id == R.id.solveButton) {
+            solve(view);
         }
         gameBoard.invalidate();
     }
